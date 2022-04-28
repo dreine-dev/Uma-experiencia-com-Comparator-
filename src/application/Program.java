a37f58d5562d3092df75e79cb5646a913cbd388a
dreine-dev <dreineantonio@hotmail.com>
2022-04-28 09:54:35 -0300

    Expressão lâmbida arrow function com chaves

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 import java.util.ArrayList;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
@@ -17,14 +18,10 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		Comparator<Product> comp = new Comparator<Product>() {
-			
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
-		
+						
 		list.sort(comp);
 		
 		for(Product p : list) {
